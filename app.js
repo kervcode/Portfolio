e11d48c557d642641e0c78f13848be3bf184c312
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2020-04-29 23:48:16 -0400

    populate data in the project page

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ const app = express();
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
+  // console.log(req)
   res.render("index", { data });
 });
+
+app.get("/project/:id", (req, res) => {
+  const id = req.params.id;
+  const project = data[id];
+  console.log(data[id])
+  res.render("project", { project });
+});
+
+app.use((err, req, res, next)=>{
+  // console.log(req.params)
+  next()
+})
 
 //static middleware
 app.use("/static", express.static("public"));
